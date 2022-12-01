8b1be78cd962ce77f738998ec9902295366ca0fe
Padraig Gleeson <p.gleeson@gmail.com>
2022-12-01 11:26:47 +0000

    Ideas

---
+++ env.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt install -y jekyll ruby-dev
+
+#gem install github-pages
+# gem install jekyll-sitemap  jekyll-seo-tag
+
+bundle install
+#bundle add webrick
+#bundle exec jekyll serve
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -it  -v $PWD:/root/local:rw  -w /root/local -p 4000:4000 ubuntu_22 /bin/bash
